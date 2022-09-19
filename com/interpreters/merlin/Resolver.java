5b58f981472d92ee4681609820451c71894a3e91
gitmskhl <gglmskhl@gmail.com>
2022-09-19 21:53:01 +0300

    super & this are completed

--- com/interpreters/merlin/Resolver.java
+++ com/interpreters/merlin/Resolver.java
@@ -97,8 +97,11 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     enum ClassType {
         NONE,
-        CLASS
+        CLASS,
+        SUBCLASS
     }
+
+    private boolean isConstructor = false;
 
     /// List -> [defined, used, initialized]
     private final Stack<Scope> scopes = new Stack<>();
@@ -202,6 +205,9 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         if (currentFunction == FunctionType.NONE) {
             Merlin.error(stmt.keyword, "Can't return from top-level code.");
         }
+        if (isConstructor && stmt.value != null) {
+            Merlin.error(stmt.keyword, "Can't return a value in the constructor.");
+        }
         if (stmt.value != null) resolve(stmt.value);
         return null;
     }
@@ -295,7 +301,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitThisExpr(ThisExpr expr) {
-        if (currentClass != ClassType.CLASS) {
+        if (currentClass == ClassType.NONE) {
             Merlin.error(expr.keyword, "Can't use 'this' outside of a class.");
         }
 
@@ -305,8 +311,11 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitSuperExpr(SuperExpr expr) {
-        if (currentClass != ClassType.CLASS) {
+        if (currentClass == ClassType.NONE) {
             Merlin.error(expr.keyword, "Can't use 'super' outside of a class.");
+        }
+        else if (currentClass != ClassType.SUBCLASS) {
+            Merlin.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
         }
         resolveLocal(expr.keyword, expr, false);
         return null;
@@ -360,8 +369,13 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         beginScope();
         scopes.peek().defineNative("this");
         ClassType tmp = currentClass;
-        currentClass = ClassType.CLASS;
-        for (Stmt.FunDeclStmt method : stmt.methods) resolve(method);
+        currentClass = stmt.superclass != null ? ClassType.SUBCLASS : ClassType.CLASS;
+        boolean previous = isConstructor;
+        for (Stmt.FunDeclStmt method : stmt.methods) {
+            isConstructor = method.name.lexeme.equals("init");
+            resolve(method);
+        }
+        isConstructor = previous;
         currentClass = tmp;
         endScope();
         if (stmt.superclass != null) {
